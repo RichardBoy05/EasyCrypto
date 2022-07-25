5a788422eebfb25aafc785c816488e110a5e4460
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2022-07-25 20:25:25 +0200

    Small bug fixes
    Small bug fixes

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,8 @@
-# from Setup import setup
-# from MainGui import init
-#
-# setup()  # sets up local and online environments for the program
-# init()   # initializes the main window
+from Setup import setup
+from MainGui import init
 
-from FirebaseUtils import user
-user(None, True)
+setup()  # sets up local and online environments for the program
+init()   # initializes the main window
+#
+# from FirebaseUtils import user
+# user(None, True)
